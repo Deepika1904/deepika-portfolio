1c1de44d5c0cd459bc86ef12451360d8e64de793
Naga Vempati <nvempati@iastate.edu>
2025-11-05 12:44:16 -0600

    build updated portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/deepika-portfolio/', // 👈 the name of your GitHub repo
   plugins: [react()],
 })
+
+  
